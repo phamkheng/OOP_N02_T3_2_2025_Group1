70f67d30d3bf2455a83ed30bff9f3ced7c6e9ec3
phamkheng <24100032@st.phenikaa-uni.edu.vn>
2025-06-23 10:06:02 +0000

    phuong  thuc cua Sathu567

--- Group1Project/src/App.java
+++ Group1Project/src/App.java
@@ -2,5 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         //Main
         System.out.println("Hello, World!");
+
+        testNumber test = new testNumber();
+
+        test.test();
+        test.testSaThu();
     }
 }
--- Group1Project/test/testNumber.java
+++ Group1Project/test/testNumber.java
@@ -18,4 +18,21 @@ public class testNumber {
 
     }
 
+    public void testSaThu() {
+
+        Number n1 = new Number();
+        Number n2 = new Number();
+        n1.i = 2;
+        n2.i = 5;
+
+        n1 = n2;
+        n2.i = 10;
+        // what is n1.i?
+        System.out.println("test cua Sa thu" + n1.i);
+
+        n1.i = 20;
+        // what is n2.i?
+        System.out.println(n2.i);
+    }
+
 }

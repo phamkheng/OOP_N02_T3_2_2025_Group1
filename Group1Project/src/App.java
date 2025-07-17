5b057a6f9a87deb78eca85e6846a3195e5d0acc0
hungspec <24100015@st.phenikaa-uni.edu.vn>
2025-07-17 08:41:41 +0000

    .

--- Group1Project/src/App.java
+++ Group1Project/src/App.java
@@ -2,11 +2,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-    Time t = new Time(20, 3, 45);
-    System.out.println(t.toString());
-    
-    Recursion r = new Recursion();
-    System.out.println(r.factorial(12));
+    TestLibraryApp.test();
 
     }
 }

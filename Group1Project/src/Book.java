90501462a72e92e3d7b89e514a0b61c846c7fd40
hungspec <24100015@st.phenikaa-uni.edu.vn>
2025-07-17 07:59:46 +0000

    .

--- Group1Project/src/Book.java
+++ Group1Project/src/Book.java
@@ -1,30 +1,11 @@
-public class Book {
+class Book {
+    String bookID;
     String title;
     String author;
-    int numPages;
-<<<<<<< HEAD
-    Book() {}
-=======
 
-    public Book() { }
-
->>>>>>> 22262b4e7c7264badbc0fd07c5e1ba51380793b7
-    public Book(String title, String author, int numPages) {
+    public Book(String bookID, String title, String author) {
+        this.bookID = bookID;
         this.title = title;
         this.author = author;
-        this.numPages = numPages;
-    }
-<<<<<<< HEAD
-    public void display() {
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
-        System.out.println("Pages: " + numPages);
     }
 }
-=======
-
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public int getNumPages() { return numPages; }
-}
->>>>>>> 22262b4e7c7264badbc0fd07c5e1ba51380793b7
---
+++ Group1Project/src/NameNumber.java
@@ -0,0 +1,17 @@
+
+public class NameNumber {
+ private String lastName; 
+ private String telNumber; 
+ public NameNumber() {} 
+ //constructor
+ public NameNumber(String name, String num) {
+ lastName = name;
+ telNumber = num;
+ }
+ // phương thức
+ public String getLastName() { return lastName;
+ }
+ public String getTelNumber() {
+ return telNumber;
+ }
+}

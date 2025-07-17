e070399d003f3b0851c5bf31c41f74fcb4ccb6f7
hungspec <24100015@st.phenikaa-uni.edu.vn>
2025-07-17 09:36:58 +0000

    .

--- Group1Project/test/TestBook.java
+++ Group1Project/test/TestBook.java
@@ -1,6 +1,6 @@
 // TestBook.java
 public class TestBook {
-    public static void main(String[] args) {
+    public static void testBook() {
         Book b = new Book("B001", "OPP", "Viet Hung");
         System.out.println("== Test Book ==");
         System.out.println("ID:     " + b.bookID);

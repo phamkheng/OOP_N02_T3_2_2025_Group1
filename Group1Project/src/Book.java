476e06eb0f199724c26acf2ea567c4cb23d6151a
phamkheng <24100032@st.phenikaa-uni.edu.vn>
2025-07-18 10:45:00 +0000

    .

--- Group1Project/src/Book.java
+++ Group1Project/src/Book.java
@@ -1,15 +1,24 @@
-class Book {
-    String bookID;
-    String title;
-    String author;
+public class Book{
+    private String bookID;
+    private String title;
+    private String author;
+    private boolean available; // Sử dụng kiểu nguyên thủy
 
-    public Book(String bookID, String title, String author) {
+    // constructor không tham số
+    public Book() {
+        this.title = "";
+        this.author = "";
+        this.bookID = "";
+        this.available = true;
+    }
+    // constructor có tham số
+    public Book(String bookID, String title, String author, int numPages, boolean available) {
         this.bookID = bookID;
         this.title = title;
         this.author = author;
+        this.available = available;
     }
-
-    public String getBookID(){
+    public String getBookID() {
         return bookID;
     }
     public String getTitle() {
@@ -17,5 +26,28 @@ class Book {
     }
     public String getAuthor() {
         return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+    // kiểm tra trạng thái của sách
+    public String checkAvailability() {
+        return available ? "có sẵn" : "trống";
+    }
+
+    public void markAsBorrowed() {
+        available = false;
+    }
+
+    public void markAsReturned() {
+        available = true;
+    }
+    // in ra màn hình thông số của sách
+    public void display() {
+        System.out.println("Tên Sách: " + title);
+        System.out.println("Mã: " + bookID);
+        System.out.println("Tác giả: " + author);
+        System.out.println("Trạng thái: " + checkAvailability());
     }
 }
--- Group1Project/src/BookCollection.java
+++
@@ -1,5 +0,0 @@
-public class BookCollection {
-    private BookInfo[] books = new BookInfo[100];
-    private int free;
-    public BookCollection() {free = 0;}
-    

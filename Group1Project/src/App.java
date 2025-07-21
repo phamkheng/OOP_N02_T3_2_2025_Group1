aadc43a88a4111d1855f559b84a72d87c8319784
hungspec <24100015@st.phenikaa-uni.edu.vn>
2025-07-21 16:54:14 +0000

    sua

--- Group1Project/src/App.java
+++ Group1Project/src/App.java
@@ -1,9 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        TestBookSearch.test();
         TestRegisterReader.test();
         TestLoanManager.test(args);
         TestReturnProcess.test();
+
 
     }
 }
